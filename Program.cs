ed006b998a04af8663dd4d6a51e962b3711c9538
trgarner <trgarner91@gmail.com>
2021-06-03 17:23:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpProject632021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
